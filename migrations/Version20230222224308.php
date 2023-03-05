fd25edb291dc32d6facf08a63a514ee3ae81a7f2
Amal-da <amal.daoud@esprit.tn>
2023-03-05 19:02:11 +0100

    integ

---
+++ migrations/Version20230222224308.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230222224308 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE commentaire (id INT AUTO_INCREMENT NOT NULL, id_annonce INT NOT NULL, nom VARCHAR(255) NOT NULL, text VARCHAR(800) NOT NULL, date DATE NOT NULL, INDEX IDX_67F068BC28C83A95 (id_annonce), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BC28C83A95 FOREIGN KEY (id_annonce) REFERENCES annonce (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BC28C83A95');
+        $this->addSql('DROP TABLE commentaire');
+    }
+}
